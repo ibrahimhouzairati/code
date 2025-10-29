f16d90732bf3b355b14ddc8c31f567e33954fbbc
ibrahimhouzairati <ibrahimhouzairati@gmail.com>
2025-10-29 12:38:18 +0100

    index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+alert("Bienvenue")
+console.log("toto")
+document.querySelector("h4").style.background="blue";
+function addition () {
+    let a = 10;
+    let b = 5;
+    return a + b;  
+}
+console.log(addition(15));
